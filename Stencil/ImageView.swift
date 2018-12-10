2cca74ac42fd58fd5e97ecd8c6744d01ca4b1299
Elvis Nuñez <elvisnunez@me.com>
2018-12-10 12:55:03 +0100

    Initial commit

---
+++ Stencil/ImageView.swift
@@ -0,0 +1,9 @@
+//
+//  ImageView.swift
+//  Stencil
+//
+//  Created by Elvis Nuñez on 10/12/2018.
+//  Copyright © 2018 FINN.no. All rights reserved.
+//
+
+import Foundation
---
+++ Stencil/Stencil.h
@@ -0,0 +1,19 @@
+//
+//  Stencil.h
+//  Stencil
+//
+//  Created by Elvis Nuñez on 10/12/2018.
+//  Copyright © 2018 FINN.no. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stencil.
+FOUNDATION_EXPORT double StencilVersionNumber;
+
+//! Project version string for Stencil.
+FOUNDATION_EXPORT const unsigned char StencilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stencil/PublicHeader.h>
+
+
